43df25bf950e59d2211364ae12e4dfede543795f
aliefdany <aliefseventri@gmail.com>
2021-08-04 20:53:22 +0700

    added hover effect for project thumbnail

--- src/ui/js/ProjectCard.js
+++ src/ui/js/ProjectCard.js
@@ -9,11 +9,51 @@ const ProjectCard = ({
   siteLink,
   tags,
 }) => {
+  let imgSelector = title.split(" ").join("-").toLowerCase();
+  let btnSelector = `${imgSelector}-btn`;
+
+  function handleClickPreview() {
+    console.log("clicked!");
+  }
+
+  function onHover() {
+    let img = document.getElementById(imgSelector);
+    let previewButton = document.getElementById(btnSelector);
+    if (!img.classList.length || !previewButton.classList.length) {
+      img.classList.add("image-hover");
+      previewButton.classList.add("button-hover");
+    }
+  }
+
+  function offHover() {
+    let img = document.getElementById(imgSelector);
+    let previewButton = document.getElementById(btnSelector);
+    img.classList.remove("image-hover");
+    previewButton.classList.remove("button-hover");
+  }
   return (
     <div className="project-card">
       <div className="project-preview">
         <div className="project-img">
-          <img src={thumb} alt="project-img" />
+          <img
+            id={title.split(" ").join("-").toLowerCase()}
+            onMouseEnter={() => {
+              onHover(title);
+            }}
+            onMouseLeave={() => {
+              offHover(title);
+            }}
+            onMouse
+            src={thumb}
+            alt="project-img"
+          />
+          <button
+            id={btnSelector}
+            onMouseEnter={onHover}
+            onClick={handleClickPreview}
+          >
+            PREVIEW
+          </button>
         </div>
         <div className="project-tags">
           {tags.map((tagObj) => {
